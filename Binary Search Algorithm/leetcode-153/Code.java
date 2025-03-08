29daad3c75c0fd59d9be8eb70865945033ea6b8e
Aditya Das <103925035+aditya301200@users.noreply.github.com>
2025-03-08 18:10:04 +0530

    leetcode-153 code added

--- Binary Search Algorithm/leetcode-153/Code.java
+++ Binary Search Algorithm/leetcode-153/Code.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int res = findMin(arr,n);
+        System.out.println("The min value is: "+res);
+    }
+
+    public static int findMin(int[] arr, int n){
+        int start = 0;
+        int end = n-1;
+        int ans = Integer.MAX_VALUE;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(arr[start]<=arr[end]){
+                ans = Math.min(arr[start],ans);
+                break;
+            }
+
+            if(arr[start] <= arr[mid]){
+                ans = Math.min(arr[start],ans);
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
 }
